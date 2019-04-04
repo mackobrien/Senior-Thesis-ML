9c32c8bd1b104595b9cb4adacd5ee5b6b209d123
Mackenzie O'Brien <mackenzieobrien@Mackenzies-MacBook-Pro.local>
2019-04-04 15:49:13 -0400

    can't get normal distribution lines to color in

--- AnalysisAverage.py
+++ AnalysisAverage.py
@@ -11,8 +11,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
+sns.set(color_codes=True)
 
-
+#import csvs
 mclAGradient = pd.read_csv('Gradient4_Satisfaction.csv')
 mclBExtreme = pd.read_csv('Extreme5_Satisfaction.csv')
 mclCExtreme = pd.read_csv('Extreme3_Satisfaction.csv')
@@ -20,13 +21,17 @@ mclCExtreme = pd.read_csv('Extreme3_Satisfaction.csv')
 #mclEDeep = pd.read_csv('Deep2_Satisfaction.csv')
 #Control = pd.read_csv('Control_Satisfaction.csv')
 
+
 #print(mclAGradient)
-sns.distplot(mclAGradient,kde=False,fit=stats.gamma,color="maroon",label="A")
+sns.distplot(mclAGradient,kde=False,fit=stats.gamma,color="red",label="A")
 sns.distplot(mclBExtreme,kde=False,fit=stats.gamma, color="darkgreen", label="B")
 sns.distplot(mclCExtreme,kde=False,fit=stats.gamma,color="deepskyblue",label="C")
+#sns.distplot(mclDDeep,kde=False,fit=stats.gamma,color="orange",label="D")
+#sns.distplot(mclEDeep,kde=False,fit=stats.gamma,color="purple",label="E")
+#sns.distplot(Control,kde=False,fit=stats.gamma, color="yellow", label="Control")
 plt.legend()
 plt.show()
-#sns.distplot(mclDDeep,kde=False,fit=stats.gamma)
-#sns.distplot(mclEDeep,kde=False,fit=stats.gamma)
 
 
+
+#calculate absolute 
