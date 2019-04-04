b731df1f34fab646aa9edfa1fca9d605f0c634b1
Mackenzie O'Brien <mackenzieobrien@Mackenzies-MacBook-Pro.local>
2019-04-04 16:10:08 -0400

    working on absolute differences

--- AnalysisAverage.py
+++ AnalysisAverage.py
@@ -14,9 +14,9 @@ from scipy import stats
 sns.set(color_codes=True)
 
 #import csvs
-mclAGradient = pd.read_csv('Gradient4_Satisfaction.csv')
-mclBExtreme = pd.read_csv('Extreme5_Satisfaction.csv')
-mclCExtreme = pd.read_csv('Extreme3_Satisfaction.csv')
+mclAGradient = pd.read_csv('Gradient4_Satisfaction.csv') #32
+mclBExtreme = pd.read_csv('Extreme5_Satisfaction.csv') #25 answers
+mclCExtreme = pd.read_csv('Extreme3_Satisfaction.csv') # 25 
 #mclDDeep = pd.read_csv('Deep1_Satisfaction.csv')
 #mclEDeep = pd.read_csv('Deep2_Satisfaction.csv')
 #Control = pd.read_csv('Control_Satisfaction.csv')
@@ -35,3 +35,33 @@ plt.show()
 
 
 #calculate absolute 
+#controlA=Control.subset(0,32)
+#ControlB=Control.subset(32,57)
+#ControlC=Control.subset(57,82)
+#ControlD=Control.subset(83,?)
+#ControlE=Control.subset(?,?)
+
+#calculate A 
+#differenceA=[];
+#for i in range(0,32):
+#    differenceA.append(abs(controlA[i]-mclAGradient[i]))
+
+#calculate B 
+#differenceB=[];
+#for i in range(0,25):
+#    differenceB.append(abs(controlB[i]-mclBExtreme[i]))
+
+#calculate C 
+#differenceC=[];
+#for i in range(0,25):
+#    differenceC.append(abs(controlC[i]-mclCExtreme[i]))
+
+#calculate D 
+#differenceD=[];
+#for i in range(0,?):
+#    differenceD.append(abs(controlD[i]-mclDDeep[i]))
+
+#calculate E 
+#differenceE=[];
+#for i in range(0,?):
+#    differenceE.append(abs(controlE[i]-mclEDeep[i]))
